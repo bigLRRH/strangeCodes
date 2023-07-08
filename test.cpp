d95e631a4423c78f7efe6bb925fdd125a7ad121f
bigLRRH <51252247+bigLRRH@users.noreply.github.com>
2023-07-08 20:00:41 +0800

    strange update

--- test.cpp
+++ test.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
-#include<chrono>
+#include <chrono>
+#include <format>
 using namespace std;
 int main()
-{   
-    cout<<chrono::system_clock::now().time_since_epoch().count()<<endl;
-    cout<<chrono::system_clock::now().time_since_epoch().count()<<endl;
-    cout<<chrono::system_clock::now().time_since_epoch().count()<<endl;
-    cout<<chrono::system_clock::now().time_since_epoch().count()<<endl;
-    cout<<chrono::system_clock::now().time_since_epoch().count();
-    cout<<chrono::system_clock::now().time_since_epoch().count();
-    cout<<chrono::system_clock::now;
-    cout<<chrono::system_clock::now;
+{
+    cout << chrono::system_clock::now().time_since_epoch().count() << endl;
+    cout << chrono::system_clock::now().time_since_epoch().count() << endl;
+    cout << chrono::system_clock::now().time_since_epoch().count() << endl;
+    cout << chrono::system_clock::now().time_since_epoch().count() << endl;
+    cout << chrono::system_clock::now().time_since_epoch().count();
+    cout << chrono::system_clock::now().time_since_epoch().count();
+    cout << chrono::system_clock::now;
+    cout << chrono::system_clock::now;
+    int i = 0;
+    cout << format("");
 }
